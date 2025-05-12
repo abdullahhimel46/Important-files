70d013ebfed5112a4663c5fa224f03b7824481b0
Md. Abdullah Himel <himel22205101622@diu.edu.bd>
2025-05-12 11:40:38 +0600

    Initial commit.

---
+++ php/test.php
@@ -0,0 +1,17 @@
+<?php
+
+// const PI = 3.1416;
+// echo "Value of PI = ".PI;
+
+// $age = "Abdullah";
+// var_dump($age);
+
+// $name="Abd";
+// $age=23;
+// printf("My name is %s and age %d.",$name,$age);
+
+$fname="md";
+$mname="Abd";
+$lname="Himel";
+
+printf('My name %3$s %1$s %2$s',$fname,$mname,$lname);
